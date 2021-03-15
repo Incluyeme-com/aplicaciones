1af2bf30e5254d99eb75bb155a26b3c1aaa11383
Jesus Nuñez <47543878+Cro22@users.noreply.github.com>
2021-03-15 11:23:55 -0400

    Ver 1.0.6

--- inclueyeme-aplicants.php
+++ inclueyeme-aplicants.php
@@ -4,7 +4,7 @@
 Plugin Name: Incluyeme Applicants
 Plugin URI: https://github.com/Cro22
 Description: Activar este plugin impide que los aplicantes puedan solicitar puestos de trabajo sin completar la sección de disCapacidades.
-Version: 1.0.6
+Version: 1.0.7
 Author: Jesus
 Author URI: https://github.com/Cro22
 Text Domain: incluyeme-applicants
